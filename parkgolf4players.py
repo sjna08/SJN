7ae375bdff1914970512ea66013b032cf127512e
sjna08 <132455968+sjna08@users.noreply.github.com>
2023-06-04 12:15:02 +0900

    Update parkgolf4players.py

--- parkgolf4players.py
+++ parkgolf4players.py
@@ -1,38 +1,49 @@
+# 필요한 라이브러리 불러오기
 import streamlit as st
 import pandas as pd
 import base64
 import io
 
+# Streamlit 앱 시작
 def app():
-    players = ['Player1', 'Player2', 'Player3', 'Player4']
-    holes = ['A1_Par4(60m)', 'A2_Par3(40m)', 'A3_Par3(65m)', 'A4_Par4(100m)', 'A5_Par5(130m)', 'A6_Par4(72m)', 'A7_Par4(81m)', 'A8_Par3(50m)', 'A9_Par3(50m)', 'B1_Par3(69m)', 'B2_Par4(100m)', 'B3_Par4(68m)', 'B4_Par3(50m)', 'B5_Par4(58m)', 'B6_Par3(55m)', 'B7_Par4(71m)', 'B8_Par5(123m)', 'B9_Par3(66m)']
+    # 플레이어 수 설정
+    num_players = 4
+    players = [st.sidebar.text_input(f'Player {i+1} 이름', value=f'Player {i+1}') for i in range(num_players)]
 
-    # Create a dictionary to keep the scores
-    scorecard = {player: {} for player in players}
+    # 홀 이름과 거리 설정
+    holes = ['A1_Par4(60m)', 'A2_Par3(40m)', 'A3_Par3(65m)', 'A4_Par4(100m)', 
+             'A5_Par5(130m)', 'A6_Par4(72m)', 'A7_Par4(81m)', 'A8_Par3(50m)', 'A9_Par3(50m)', 
+             'B1_Par3(69m)', 'B2_Par4(100m)', 'B3_Par4(68m)', 'B4_Par3(50m)', 
+             'B5_Par4(58m)', 'B6_Par3(55m)', 'B7_Par4(71m)', 'B8_Par5(123m)', 'B9_Par3(66m)']
 
-    for player in players:
-        st.sidebar.header(player)
-        for hole in holes:
-            scorecard[player][hole] = st.sidebar.number_input(f'{player} {hole} Score', min_value=0, value=0)
+    # 스코어카드 생성
+    scorecard = pd.DataFrame(index=players, columns=holes)
 
-    if st.button('Submit'):
-        scorecard_df = pd.DataFrame(scorecard)
+    # 사용자에게 입력 받기
+    for hole in holes:
+        st.subheader(hole)
+        for player in players:
+            scorecard.loc[player, hole] = st.number_input(f'{player} {hole} 점수', min_value=0, value=0, key=f'{player}_{hole}')
 
-        # Calculate totals
-        total = scorecard_df.sum().rename('Total')
-        scorecard_df = scorecard_df.append(total)
+    # 입력 받은 정보를 표시하고 다운로드 받기
+    if st.button('제출'):
+        summary = pd.DataFrame(index=players, columns=['Total', 'A_total', 'B_total'])
+        summary['A_total'] = scorecard.iloc[:, :9].sum(axis=1)
+        summary['B_total'] = scorecard.iloc[:, 9:].sum(axis=1)
+        summary['Total'] = summary['A_total'] + summary['B_total']
 
-        A_total = scorecard_df[holes[:9]].sum(axis=1).rename('A Total')
-        B_total = scorecard_df[holes[9:]].sum(axis=1).rename('B Total')
+        st.write(summary)
+        st.write(scorecard)
 
-        scorecard_df = pd.concat([scorecard_df, A_total, B_total], axis=1)
+        # 스코어카드와 요약을 합치기
+        full_scorecard = pd.concat([summary, scorecard], axis=1)
 
-        st.write(scorecard_df.T)
-
-        csv = scorecard_df.to_csv(index=False)
-        b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-        href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a>'
+        csv_buffer = io.StringIO()
+        full_scorecard.to_csv(csv_buffer, index=True, encoding='utf-8-sig')
+        csv_string = csv_buffer.getvalue()
+        b64 = base64.b64encode(csv_string.encode()).decode() 
+        href = f'<a href="data:file/csv;base64,{b64}" download="scorecard.csv">Download CSV File</a>'
         st.markdown(href, unsafe_allow_html=True)
 
-
-app()
+if __name__ == "__main__":
+    app()
