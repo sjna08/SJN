dfb5c89a12ce00be990fa88dc358fa76dd1e56fe
sjna08 <132455968+sjna08@users.noreply.github.com>
2023-06-05 00:34:46 +0900

    Update parkgolf4players.py

--- parkgolf4players.py
+++ parkgolf4players.py
@@ -6,24 +6,14 @@ import io
 
 # Main Page
 st.title("ParkGolf ScoreCard")
+
 # Streamlit 앱 시작
 def app():
-# 홀 이름과 거리 설정
-holes = ['A1_Par4(60m)', 'A2_Par3(40m)', 'A3_Par3(65m)', 'A4_Par4(100m)', 
-         'A5_Par5(130m)', 'A6_Par4(72m)', 'A7_Par4(81m)', 'A8_Par3(50m)', 'A9_Par3(50m)', 
-         'B1_Par3(69m)', 'B2_Par4(100m)', 'B3_Par4(68m)', 'B4_Par3(50m)', 
-         'B5_Par4(58m)', 'B6_Par3(55m)', 'B7_Par4(71m)', 'B8_Par5(123m)', 'B9_Par3(66m)']
 
     # 플레이어 수 설정
     num_players = 4
     players = [st.sidebar.text_input(f'Player {i+1}이름',value=f'Player{i+1}') for i in range(num_players)]
   
-    for i, player in enumerate(players):
-      for hole in holes:
-          key = f"{player}_{hole}_{i}"  # player index 추가
-          scorecard.loc[player, hole] = st.number_input(f'{player} {hole} 점수', min_value=0, value=0, key=key)
-
-
     # 홀 이름과 거리 설정
     holes = ['A1_Par4(60m)', 'A2_Par3(40m)', 'A3_Par3(65m)', 'A4_Par4(100m)', 
              'A5_Par5(130m)', 'A6_Par4(72m)', 'A7_Par4(81m)', 'A8_Par3(50m)', 'A9_Par3(50m)', 
