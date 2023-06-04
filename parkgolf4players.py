7eccfed6f0c66443aa165a84118c0bf23cc47c68
sjna08 <132455968+sjna08@users.noreply.github.com>
2023-06-04 13:11:01 +0900

    Update parkgolf4players.py

--- parkgolf4players.py
+++ parkgolf4players.py
@@ -27,10 +27,15 @@ def app():
 
     # 입력 받은 정보를 표시하고 다운로드 받기
     if st.button('제출'):
-        summary = pd.DataFrame(index=players, columns=['Total', 'A_total', 'B_total'])
+        summary = pd.DataFrame(index=players, columns=['Total', 'A_total', 'B_total', 'A_Diff', 'B_Diff', 'Total_Diff'])
         summary['A_total'] = scorecard.iloc[:, :9].sum(axis=1)
         summary['B_total'] = scorecard.iloc[:, 9:].sum(axis=1)
         summary['Total'] = summary['A_total'] + summary['B_total']
+
+        # 차이를 계산
+        summary['A_Diff'] = summary['A_total'] - 33
+        summary['B_Diff'] = summary['B_total'] - 33
+        summary['Total_Diff'] = summary['Total'] - 66
 
         st.write(summary)
         st.write(scorecard)
