ee6e16f11846875fd5843df1aa97f7404b690866
sjna08 <132455968+sjna08@users.noreply.github.com>
2023-06-07 01:45:15 +0900

    Update Parkgolf36holes_02.py

--- Parkgolf36holes_02.py
+++ Parkgolf36holes_02.py
@@ -1,13 +1,13 @@
-# 필요한 라이브러리 불러오기
+# Import necessary libraries
 import streamlit as st
 import pandas as pd
 import base64
 import io
 
-# Main Page
+# Main title
 st.title("ParkGolf ScoreCard")
 
-# 홀 이름과 거리 설정
+# Hole names and distances
 holes = ['A1_Par4(60m)', 'A2_Par3(40m)', 'A3_Par3(65m)', 'A4_Par4(100m)', 
          'A5_Par5(130m)', 'A6_Par4(72m)', 'A7_Par4(81m)', 'A8_Par3(50m)', 'A9_Par3(50m)', 
          'B1_Par3(69m)', 'B2_Par4(100m)', 'B3_Par4(68m)', 'B4_Par3(50m)', 
@@ -17,58 +17,35 @@ holes = ['A1_Par4(60m)', 'A2_Par3(40m)', 'A3_Par3(65m)', 'A4_Par4(100m)',
          'D1_Par3(69m)', 'D2_Par4(100m)', 'D3_Par4(68m)', 'D4_Par3(50m)', 
          'D5_Par4(58m)', 'D6_Par3(55m)', 'D7_Par4(71m)', 'D8_Par5(123m)', 'D9_Par3(66m)']
 
-# 플레이어 수 설정
+# Number of players
 num_players = 4
-players = [st.sidebar.text_input(f'Player {i+1}이름',value=f'Player{i+1}') for i in range(num_players)]
+players = [st.sidebar.text_input(f'Player {i+1} name',value=f'Player{i+1}') for i in range(num_players)]
 
-# 페이지 설정
-page = st.sidebar.radio('페이지 선택', ['A&B 홀', 'C&D 홀', '전체'])
+# Page selection
+page = st.sidebar.radio('Choose page', ['A&B Holes', 'C&D Holes', 'All'])
 
-if page == 'A&B 홀':
+if page == 'A&B Holes':
     selected_holes = holes[:18]
-elif page == 'C&D 홀':
+elif page == 'C&D Holes':
     selected_holes = holes[18:]
 else:
     selected_holes = holes
 
-# 스코어카드 생성
+# Create scorecard
 if 'scorecard' not in st.session_state:
     st.session_state['scorecard'] = pd.DataFrame(index=players, columns=holes)
 scorecard = st.session_state['scorecard']
 
-# 사용자에게 입력 받기
+# Get input from users
 for hole in selected_holes:
     st.subheader(hole)
     for player in players:
-        scorecard.loc[player, hole] = st.number_input(f'{player} {hole} 점수', min_value=0, value=0, key=f'{player}_{hole}')
+        scorecard.loc[player, hole] = st.number_input(f'{player} score for {hole}', min_value=0, value=0, key=f'{player}_{hole}')
 
-# 입력 받은 정보를 표시하고 다운로드 받기
-if st.button('제출'):
+# Show input and provide download
+if st.button('Submit'):
     summary = pd.DataFrame(index=players, columns=['TTL','A','B', 'A_Dif','B_Dif','C','D', 'C_Dif','D_Dif','TTL_Dif'])
     summary['A'] = scorecard.iloc[:, :9].sum(axis=1)
-    summary['B'] = scorecard.iloc[:, 9:18].sum(axis=1)
-    summary['C'] = scorecard.iloc[:, 18:27].sum(axis=1)
-    summary['D'] = scorecard.iloc[:, 27:].sum(axis=1)
-    summary['TTL'] = summary['A'] + summary['B'] + summary['C'] + summary['D']
-    
-    # 차이를 계산
-    summary['A_Dif'] = summary['A'] - 33
-    summary['B_Dif'] = summary['B'] - 33
-    summary['C_Dif'] = summary['C'] - 33
-    summary['D_Dif'] = summary['D'] - 33
-    summary['TTL_Dif'] = summary['TTL'] - 132
-
-    st.write(summary)
-    st.write(scorecard)
-
-    # 스코어카드와 요약을 합치기
-    full_scorecard = pd.concat([summary, scorecard], axis=1)
-
-    csv_buffer = io.StringIO()
-    full_scorecard.to_csv(csv_buffer, index=True, encoding='utf-8-sig')
-    csv_string = csv_buffer.getvalue()
-    b64 = base64.b64encode(csv_string.encode()).decode() 
-    href = f'<a href="data:file/csv;base64,{b64}" download="scorecard.csv">Download CSV File</a>'
-    st.markdown(href, unsafe_allow_html=True)
+    summary['B'] = scorecard.iloc[:, 9:
 
  
