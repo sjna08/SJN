45ef78f127962fe01371c277bdfa5d9277879d15
sjna08 <132455968+sjna08@users.noreply.github.com>
2023-06-07 01:06:15 +0900

    Update Parkgolf36holes_02.py

--- Parkgolf36holes_02.py
+++ Parkgolf36holes_02.py
@@ -19,7 +19,7 @@ holes = ['A1_Par4(60m)', 'A2_Par3(40m)', 'A3_Par3(65m)', 'A4_Par4(100m)',
 
 # 플레이어 수 설정
 num_players = 4
-players = [st.sidebar.text_input(f'Player {i+1}이름',value=f'Player{i+1}') for i in range(num_players)]
+players = [st.sidebar.text_input(f'Player {i+1} 이름', value='') for i in range(num_players)]
 
 # 페이지 설정
 page = st.sidebar.radio('페이지 선택', ['A&B 홀', 'C&D 홀', '전체'])
@@ -40,33 +40,6 @@ scorecard = st.session_state['scorecard']
 for hole in selected_holes:
     st.subheader(hole)
     for player in players:
-        scorecard.loc[player, hole] = st.number_input(f'{player} {hole} 점수', min_value=0, value=0, key=f'{player}_{hole}')
+        scorecard.loc[player, hole] = st.number_input(f'{player} {hole} 점수', min_value=0, value=scorecard.loc[player, hole], key=f'{player}_{hole}')
 
 # 입력 받은 정보를 표시하고 다운로드 받기
-if st.button('제출'):
-    summary = pd.DataFrame(index=players, columns=['TTL','A','B', 'A_Dif','B_Dif','C','D', 'C_Dif','D_Dif','TTL_Dif'])
-    summary['A'] = scorecard.iloc[:, :9].sum(axis=1)
-    summary['B'] = scorecard.iloc[:, 9:18].sum(axis=1)
-    summary['C'] = scorecard.iloc[:, 18:27].sum(axis=1)
-    summary['D'] = scorecard.iloc[:, 27:].sum(axis=1)
-    summary['TTL'] = summary['A'] + summary['B'] + summary['C'] + summary['D']
-    
-    # 차이를 계산
-    summary['A_Dif'] = summary['A'] - 33
-    summary['B_Dif'] = summary['B'] - 33
-    summary['C_Dif'] = summary['C'] - 33
-    summary['D_Dif'] = summary['D'] - 33
-    summary['TTL_Dif'] = summary['TTL'] - 132
-
-    st.write(summary)
-    st.write(scorecard)
-
-    # 스코어카드와 요약을 합치기
-    full_scorecard = pd.concat([summary, scorecard], axis=1)
-
-    csv_buffer = io.StringIO()
-    full_scorecard.to_csv(csv_buffer, index=True, encoding='utf-8-sig')
-    csv_string = csv_buffer.getvalue()
-    b64 = base64.b64encode(csv_string.encode()).decode() 
-    href = f'<a href="data:file/csv;base64,{b64}" download="scorecard.csv">Download CSV File</a>'
-    st.markdown(href, unsafe_allow_html=True)
