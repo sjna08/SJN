ce77833812fa94d0782ff957a0a8c3e521d01b89
sjna08 <132455968+sjna08@users.noreply.github.com>
2023-06-07 01:30:57 +0900

    Update Parkgolf36holes_02.py

--- Parkgolf36holes_02.py
+++ Parkgolf36holes_02.py
@@ -71,10 +71,10 @@ if st.button('제출'):
     href = f'<a href="data:file/csv;base64,{b64}" download="scorecard.csv">Download CSV File</a>'
     st.markdown(href, unsafe_allow_html=True)
 
-# 사용자에게 입력 받기
-for hole in selected_holes:
-    st.subheader(hole)
-    for player in players:
+    # 사용자에게 입력 받기
+    for hole in selected_holes:
+       st.subheader(hole)
+       for player in players:
         score = scorecard.loc[player, hole]
         score = 0 if pd.isnull(score) else int(score)
         scorecard.loc[player, hole] = st.number_input(f'{player} {hole} 점수', min_value=0, value=score, key=f'{player}_{hole}_{page}')
