3ca0d7e8c7bb25f74a9864df408dec2a6b119cd2
sjna08 <132455968+sjna08@users.noreply.github.com>
2023-06-07 01:20:42 +0900

    Update Parkgolf36holes_02.py

--- Parkgolf36holes_02.py
+++ Parkgolf36holes_02.py
@@ -40,6 +40,6 @@ scorecard = st.session_state['scorecard']
 for hole in selected_holes:
     st.subheader(hole)
     for player in players:
-        if pd.isnull(scorecard.loc[player, hole]):
-            scorecard.loc[player, hole] = 0
-        scorecard.loc[player, hole] = st.number_input(f'{player} {hole} 점수', min_value=0, value=int(scorecard.loc[player, hole]), key=f'{player}_{hole}_{page}')
+        score = scorecard.loc[player, hole]
+        score = 0 if pd.isnull(score) else int(score)
+        scorecard.loc[player, hole] = st.number_input(f'{player} {hole} 점수', min_value=0, value=score, key=f'{player}_{hole}_{page}')
