bf9208ac6a9075eb2c1982a302c7bd16580578ea
sjna08 <132455968+sjna08@users.noreply.github.com>
2023-06-07 15:10:29 +0900

    Update Parkgolf36holes_02.py

--- Parkgolf36holes_02.py
+++ Parkgolf36holes_02.py
@@ -1,6 +1,3 @@
-죄송합니다, 제가 올바르게 코드를 작성하지 않았습니다. Python은 indentation을 매우 엄격하게 취급하는 언어입니다. 주석에 쓰인 `#...`은 실제 코드로 대체되어야 하는 부분을 나타내는 데 사용된 것입니다. 여기서 `제출` 버튼이 클릭되었을 때 실행되어야 하는 코드를 넣어야 합니다. 아래에 수정된 코드를 제공하겠습니다.
-
-```python
 # 필요한 라이브러리 불러오기
 import streamlit as st
 import pandas as pd
@@ -12,12 +9,17 @@ st.title("ParkGolf ScoreCard")
 
 # 홀 이름과 거리 설정
 holes = ['A1_Par4(60m)', 'A2_Par3(40m)', 'A3_Par3(65m)', 'A4_Par4(100m)', 
-         # ... (추가로 필요한 홀들)
-        ]
+         'A5_Par5(130m)', 'A6_Par4(72m)', 'A7_Par4(81m)', 'A8_Par3(50m)', 'A9_Par3(50m)', 
+         'B1_Par3(69m)', 'B2_Par4(100m)', 'B3_Par4(68m)', 'B4_Par3(50m)', 
+         'B5_Par4(58m)', 'B6_Par3(55m)', 'B7_Par4(71m)', 'B8_Par5(123m)', 'B9_Par3(66m)',
+         'C1_Par4(60m)', 'C2_Par3(40m)', 'C3_Par3(65m)', 'C4_Par4(100m)', 
+         'C5_Par5(130m)', 'C6_Par4(72m)', 'C7_Par4(81m)', 'C8_Par3(50m)', 'C9_Par3(50m)',
+         'D1_Par3(69m)', 'D2_Par4(100m)', 'D3_Par4(68m)', 'D4_Par3(50m)', 
+         'D5_Par4(58m)', 'D6_Par3(55m)', 'D7_Par4(71m)', 'D8_Par5(123m)', 'D9_Par3(66m)']
 
 # 플레이어 수 설정
 num_players = 4
-players = [st.sidebar.text_input(f'Player {i+1}이름',value=f'Player{i+1}') for i in range(num_players)]
+players = [st.sidebar.text_input(f'Player {i+1} 이름', value=f'Player{i+1}') for i in range(num_players)]
 
 # 페이지 설정
 page = st.sidebar.radio('페이지 선택', ['A&B 홀', 'C&D 홀', '전체'])
@@ -32,19 +34,38 @@ else:
 # 스코어카드 생성
 if 'scorecard' not in st.session_state:
     st.session_state['scorecard'] = pd.DataFrame(index=players, columns=holes)
-else:
-    st.session_state['scorecard'].index = players  # update player names in scorecard
 scorecard = st.session_state['scorecard']
 
 # 사용자에게 입력 받기
-for hole in selected_holes:
-    st.subheader(hole)
-    for player in players:
+for player in players:
+    for hole in selected_holes:
         scorecard.loc[player, hole] = st.number_input(f'{player} {hole} 점수', min_value=0, value=0, key=f'{player}_{hole}')
 
 # 입력 받은 정보를 표시하고 다운로드 받기
 if st.button('제출'):
-    # 작업을 계속하기 전에 결과를 계산하고 출력하는 실제 코드를 여기에 작성해야 합니다.
-    pass
-```
-위 코드의 마지막 부분에서 '제출' 버튼이 클릭되었을 때 수행해야 하는 작업을 추가하시면 됩니다. 이 경우에는 플레이어의 점수를 계산하고 결과를 출력하는 코드를 추가해야 합니다.
+    summary = pd.DataFrame(index=players, columns=['TTL','A','B', 'A_Dif','B_Dif','C','D', 'C_Dif','D_Dif','TTL_Dif'])
+    summary['A'] = scorecard.iloc[:, :9].sum(axis=1)
+    summary['B'] = scorecard.iloc[:, 9:18].sum(axis=1)
+    summary['C'] = scorecard.iloc[:, 18:27].sum(axis=1)
+    summary['D'] = scorecard.iloc[:, 27:].sum(axis=1)
+    summary['TTL'] = summary['A'] + summary['B'] + summary['C'] + summary['D']
+    
+    # 차이를 계산
+    summary['A_Dif'] = summary['A'] - 33
+    summary['B_Dif'] = summary['B'] - 33
+    summary['C_Dif'] = summary['C'] - 33
+    summary['D_Dif'] = summary['D'] - 33
+    summary['TTL_Dif'] = summary['TTL'] - 132
+
+    st.write(summary)
+    st.write(scorecard)
+
+    # 스코어카드와 요약을 합치기
+    full_scorecard = pd.concat([summary, scorecard], axis=1)
+
+    csv_buffer = io.StringIO()
+    full_scorecard.to_csv(csv_buffer, index=True, encoding='utf-8-sig')
+    csv_string = csv_buffer.getvalue()
+    b64 = base64.b64encode(csv_string.encode()).decode() 
+    href = f'<a href="data:file/csv;base64,{b64}" download="scorecard.csv">Download CSV File</a>'
+    st.markdown(href, unsafe_allow_html=True)
