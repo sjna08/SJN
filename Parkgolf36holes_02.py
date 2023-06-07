a8d60c3a03df820d9578b6d4b64af0139e02e88e
sjna08 <132455968+sjna08@users.noreply.github.com>
2023-06-07 15:20:40 +0900

    Update Parkgolf36holes_02.py

--- Parkgolf36holes_02.py
+++ Parkgolf36holes_02.py
@@ -19,7 +19,7 @@ holes = ['A1_Par4(60m)', 'A2_Par3(40m)', 'A3_Par3(65m)', 'A4_Par4(100m)',
 
 # 플레이어 수 설정
 num_players = 4
-players = [st.sidebar.text_input(f'Player {i+1} 이름', value=f'Player{i+1}') for i in range(num_players)]
+players = [st.sidebar.text_input(f'Player {i+1}이름',value=f'Player{i+1}') for i in range(num_players)]
 
 # 페이지 설정
 page = st.sidebar.radio('페이지 선택', ['A&B 홀', 'C&D 홀', '전체'])
@@ -37,8 +37,9 @@ if 'scorecard' not in st.session_state:
 scorecard = st.session_state['scorecard']
 
 # 사용자에게 입력 받기
-for player in players:
-    for hole in selected_holes:
+for hole in selected_holes:
+    st.subheader(hole)
+    for player in players:
         scorecard.loc[player, hole] = st.number_input(f'{player} {hole} 점수', min_value=0, value=0, key=f'{player}_{hole}')
 
 # 입력 받은 정보를 표시하고 다운로드 받기
