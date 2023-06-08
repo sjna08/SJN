8188d1004a07dcf7a2395c41a9a02f5f027d6204
sjna08 <132455968+sjna08@users.noreply.github.com>
2023-06-09 00:39:03 +0900

    Update Parkgolf36holes_08.py

--- Parkgolf36holes_08.py
+++ Parkgolf36holes_08.py
@@ -3,68 +3,61 @@ import pandas as pd
 import base64
 import io
 
-# Main Page
 st.title("ParkGolf ScoreCard")
 
-# Streamlit 앱 시작
 def app():
-    # 플레이어 수 설정
+
     num_players = 4
-    players = [st.sidebar.text_input(f'Player {i+1} 이름', value=f'Player{i+1}') for i in range(num_players)]
+    players = [st.sidebar.text_input(f'Player {i+1}이름',value=f'Player{i+1}') for i in range(num_players)]
+  
+    page = st.sidebar.radio('페이지 선택', ['A 홀', 'B 홀', 'C 홀', 'D 홀', '전체'])
 
-    # 홀 이름과 거리 설정
-    holes = ['A1_Par4(60m)', 'A2_Par3(40m)', 'A3_Par3(65m)', 'A4_Par4(100m)', 
-             'A5_Par5(130m)', 'A6_Par4(72m)', 'A7_Par4(81m)', 'A8_Par3(50m)', 'A9_Par3(50m)', 
-             'B1_Par3(69m)', 'B2_Par4(100m)', 'B3_Par4(68m)', 'B4_Par3(50m)', 
-             'B5_Par4(58m)', 'B6_Par3(55m)', 'B7_Par4(71m)', 'B8_Par5(123m)', 'B9_Par3(66m)',
-             'C1_Par4(60m)', 'C2_Par3(40m)', 'C3_Par3(65m)', 'C4_Par4(100m)', 
-             'C5_Par5(130m)', 'C6_Par4(72m)', 'C7_Par4(81m)', 'C8_Par3(50m)', 'C9_Par3(50m)',
-             'D1_Par3(69m)', 'D2_Par4(100m)', 'D3_Par4(68m)', 'D4_Par3(50m)', 
-             'D5_Par4(58m)', 'D6_Par3(55m)', 'D7_Par4(71m)', 'D8_Par5(123m)', 'D9_Par3(66m)']
+    holes = {
+        'A 홀': ['A1_Par4(60m)', 'A2_Par3(40m)', 'A3_Par3(65m)', 'A4_Par4(100m)', 'A5_Par5(130m)', 'A6_Par4(72m)', 'A7_Par4(81m)', 'A8_Par3(50m)', 'A9_Par3(50m)'],
+        'B 홀': ['B1_Par3(69m)', 'B2_Par4(100m)', 'B3_Par4(68m)', 'B4_Par3(50m)', 'B5_Par4(58m)', 'B6_Par3(55m)', 'B7_Par4(71m)', 'B8_Par5(123m)', 'B9_Par3(66m)'],
+        'C 홀': ['C1_Par4(60m)', 'C2_Par3(40m)', 'C3_Par3(65m)', 'C4_Par4(100m)', 'C5_Par5(130m)', 'C6_Par4(72m)', 'C7_Par4(81m)', 'C8_Par3(50m)', 'C9_Par3(50m)'],
+        'D 홀': ['D1_Par3(69m)', 'D2_Par4(100m)', 'D3_Par4(68m)', 'D4_Par3(50m)', 'D5_Par4(58m)', 'D6_Par3(55m)', 'D7_Par4(71m)', 'D8_Par5(123m)', 'D9_Par3(66m)'],
+        '전체': ['A1_Par4(60m)', 'A2_Par3(40m)', 'A3_Par3(65m)', 'A4_Par4(100m)', 'A5_Par5(130m)', 'A6_Par4(72m)', 'A7_Par4(81m)', 'A8_Par3(50m)', 'A9_Par3(50m)',
+                 'B1_Par3(69m)', 'B2_Par4(100m)', 'B3_Par4(68m)', 'B4_Par3(50m)', 'B5_Par4(58m)', 'B6_Par3(55m)', 'B7_Par4(71m)', 'B8_Par5(123m)', 'B9_Par3(66m)',
+                 'C1_Par4(60m)', 'C2_Par3(40m)', 'C3_Par3(65m)', 'C4_Par4(100m)', 'C5_Par5(130m)', 'C6_Par4(72m)', 'C7_Par4(81m)', 'C8_Par3(50m)', 'C9_Par3(50m)',
+                 'D1_Par3(69m)', 'D2_Par4(100m)', 'D3_Par4(68m)', 'D4_Par3(50m)', 'D5_Par4(58m)', 'D6_Par3(55m)', 'D7_Par4(71m)', 'D8_Par5(123m)', 'D9_Par3(66m)']
+    }
 
-    # 스코어카드 생성 또는 불러오기
-    if 'scorecard' not in st.session_state:
-        st.session_state['scorecard'] = pd.DataFrame(index=players, columns=holes)
-    elif st.button("저장된 점수카드 불러오기"):
-        st.session_state['scorecard'] = pd.read_csv('scorecard.csv', index_col=0)
+    if page == '전체':
+        selected_holes = holes['전체']
+    else:
+        selected_holes = holes[page]
 
-    # 입력된 점수를 표시하고 저장
-    scorecard = st.session_state['scorecard']
-    for hole in holes:
+    scorecard = pd.DataFrame(index=players, columns=selected_holes)
+
+    for hole in selected_holes:
         st.subheader(hole)
         for player in players:
-            default_value = scorecard.loc[player, hole] if not pd.isna(scorecard.loc[player, hole]) else 0
-            score = st.number_input(f'{player} {hole} 점수', min_value=0, value=int(default_value), key=f'{player}_{hole}', format="%d")
-            scorecard.loc[player, hole] = score
+            scorecard.loc[player, hole] = st.number_input(f'{player} {hole} 점수', min_value=0, value=0, key=f'{player}_{hole}')
 
-    # 입력된 점수를 저장
-    st.session_state['scorecard'] = scorecard
-
-    # Submit scores and calculate summary
     if st.button('제출'):
-        summary = pd.DataFrame(index=players, columns=['TTL','A','B', 'C', 'D', 'A_Dif','B_Dif','C_Dif','D_Dif','TTL_Dif'])
+        summary = pd.DataFrame(index=players, columns=['TTL', 'A', 'B', 'A_Dif', 'B_Dif', 'C', 'D', 'C_Dif', 'D_Dif', 'TTL_Dif'])
         summary['A'] = scorecard.iloc[:, :9].sum(axis=1)
         summary['B'] = scorecard.iloc[:, 9:18].sum(axis=1)
         summary['C'] = scorecard.iloc[:, 18:27].sum(axis=1)
         summary['D'] = scorecard.iloc[:, 27:].sum(axis=1)
         summary['TTL'] = summary['A'] + summary['B'] + summary['C'] + summary['D']
-
         summary['A_Dif'] = summary['A'] - 33
         summary['B_Dif'] = summary['B'] - 33
         summary['C_Dif'] = summary['C'] - 33
         summary['D_Dif'] = summary['D'] - 33
         summary['TTL_Dif'] = summary['TTL'] - 132
 
-        st.write(summary.fillna(0).astype(int))
-        st.write(scorecard.fillna(0).astype(int))
+        st.write(summary)
+        st.write(scorecard)
 
         full_scorecard = pd.concat([summary, scorecard], axis=1)
 
         csv_buffer = io.StringIO()
-        full_scorecard.fillna(0).astype(int).to_csv(csv_buffer, index=True, encoding='utf-8-sig')
+        full_scorecard.to_csv(csv_buffer, index=True, encoding='utf-8-sig')
         csv_string = csv_buffer.getvalue()
         b64 = base64.b64encode(csv_string.encode()).decode() 
-        href = f'<a href="data:file/csv;base64,{b64}" download="scorecard.csv">점수카드 열기</a>'
+        href = f'<a href="data:file/csv;base64,{b64}" download="scorecard.csv">Download CSV File</a>'
         st.markdown(href, unsafe_allow_html=True)
 
         if st.button("점수카드 저장"):
@@ -73,4 +66,3 @@ def app():
 
 if __name__ == "__main__":
     app()
-
