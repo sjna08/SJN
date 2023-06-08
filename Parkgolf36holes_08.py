1322cfdfc695a139893465cd267ccb33d2d04799
sjna08 <132455968+sjna08@users.noreply.github.com>
2023-06-09 00:18:56 +0900

    Update Parkgolf36holes_08.py

--- Parkgolf36holes_08.py
+++ Parkgolf36holes_08.py
@@ -6,47 +6,41 @@ import io
 st.title("ParkGolf ScoreCard")
 
 def app():
+
     num_players = 4
-    players = [st.sidebar.text_input(f'Player {i+1} 이름', value=f'Player{i+1}') for i in range(num_players)]
-
-    page = st.sidebar.radio('페이지 선택', ['A 홀', 'B 홀', 'C 홀', 'D 홀'])
-
-    if page == 'A 홀':
-        holes = ['A1_Par4(60m)', 'A2_Par3(40m)', 'A3_Par3(65m)', 'A4_Par4(100m)',
-                 'A5_Par5(130m)', 'A6_Par4(72m)', 'A7_Par4(81m)', 'A8_Par3(50m)', 'A9_Par3(50m)']
-    elif page == 'B 홀':
-        holes = ['B1_Par3(69m)', 'B2_Par4(100m)', 'B3_Par4(68m)', 'B4_Par3(50m)',
-                 'B5_Par4(58m)', 'B6_Par3(55m)', 'B7_Par4(71m)', 'B8_Par5(123m)', 'B9_Par3(66m)']
-    elif page == 'C 홀':
-        holes = ['C1_Par4(60m)', 'C2_Par3(40m)', 'C3_Par3(65m)', 'C4_Par4(100m)',
-                 'C5_Par5(130m)', 'C6_Par4(72m)', 'C7_Par4(81m)', 'C8_Par3(50m)', 'C9_Par3(50m)']
-    else:
-        holes = ['D1_Par3(69m)', 'D2_Par4(100m)', 'D3_Par4(68m)', 'D4_Par3(50m)',
-                 'D5_Par4(58m)', 'D6_Par3(55m)', 'D7_Par4(71m)', 'D8_Par5(123m)', 'D9_Par3(66m)']
+    players = [st.sidebar.text_input(f'Player {i+1}이름',value=f'Player{i+1}') for i in range(num_players)]
+  
+    holes = ['A1_Par4(60m)', 'A2_Par3(40m)', 'A3_Par3(65m)', 'A4_Par4(100m)', 
+             'A5_Par5(130m)', 'A6_Par4(72m)', 'A7_Par4(81m)', 'A8_Par3(50m)', 'A9_Par3(50m)', 
+             'B1_Par3(69m)', 'B2_Par4(100m)', 'B3_Par4(68m)', 'B4_Par3(50m)', 
+             'B5_Par4(58m)', 'B6_Par3(55m)', 'B7_Par4(71m)', 'B8_Par5(123m)', 'B9_Par3(66m)',
+             'C1_Par4(60m)', 'C2_Par3(40m)', 'C3_Par3(65m)', 'C4_Par4(100m)', 
+             'C5_Par5(130m)', 'C6_Par4(72m)', 'C7_Par4(81m)', 'C8_Par3(50m)', 'C9_Par3(50m)',
+             'D1_Par3(69m)', 'D2_Par4(100m)', 'D3_Par4(68m)', 'D4_Par3(50m)', 
+             'D5_Par4(58m)', 'D6_Par3(55m)', 'D7_Par4(71m)', 'D8_Par5(123m)', 'D9_Par3(66m)']
 
     scorecard = pd.DataFrame(index=players, columns=holes)
 
     for hole in holes:
         st.subheader(hole)
         for player in players:
-            scorecard.loc[player, hole] = st.number_input(f'{player} {hole} 점수', min_value=0, value=0, key=f'{player}_{hole}')
+            if not pd.isna(scorecard.loc[player, hole]):
+                score = scorecard.loc[player, hole]
+            else:
+                score = 0
+            input_score = st.number_input(f'{player} {hole} 점수', min_value=0, value=score, key=f'{player}_{hole}')
+            scorecard.loc[player, hole] = input_score
 
     if st.button('제출'):
-        summary = pd.DataFrame(index=players, columns=['TTL','A','B', 'A_Dif','B_Dif','C','D', 'C_Dif','D_Dif','TTL_Dif'])
+        summary = pd.DataFrame(index=players, columns=['TTL','A','B', 'C','D'])
         summary['A'] = scorecard.iloc[:, :9].sum(axis=1)
         summary['B'] = scorecard.iloc[:, 9:18].sum(axis=1)
         summary['C'] = scorecard.iloc[:, 18:27].sum(axis=1)
         summary['D'] = scorecard.iloc[:, 27:].sum(axis=1)
         summary['TTL'] = summary['A'] + summary['B'] + summary['C'] + summary['D']
-        
-        summary['A_Dif'] = summary['A'] - 33
-        summary['B_Dif'] = summary['B'] - 33
-        summary['C_Dif'] = summary['C'] - 33
-        summary['D_Dif'] = summary['D'] - 33
-        summary['TTL_Dif'] = summary['TTL'] - 132
 
-        st.write(summary)
-        st.write(scorecard)
+        st.write(summary.astype(int))
+        st.write(scorecard.astype(int))
 
         full_scorecard = pd.concat([summary, scorecard], axis=1)
 
@@ -56,6 +50,10 @@ def app():
         b64 = base64.b64encode(csv_string.encode()).decode() 
         href = f'<a href="data:file/csv;base64,{b64}" download="scorecard.csv">Download CSV File</a>'
         st.markdown(href, unsafe_allow_html=True)
+
+        if st.button("점수카드 저장"):
+            with open('scorecard.csv', 'w') as f:
+                f.write(csv_string)
 
 if __name__ == "__main__":
     app()
