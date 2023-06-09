9c0100cfa26a10f542077156ea98974243892a62
sjna08 <132455968+sjna08@users.noreply.github.com>
2023-06-09 09:45:55 +0900

    Update Parkgolf36holes_08_01.py

--- Parkgolf36holes_08_01.py
+++ Parkgolf36holes_08_01.py
@@ -13,61 +13,54 @@ def app():
     players = [st.sidebar.text_input(f'Player {i+1} 이름', value=f'Player{i+1}') for i in range(num_players)]
 
     # 홀 이름과 거리 설정
-    holes = ['A1_Par4(60m)', 'A2_Par3(40m)', 'A3_Par3(65m)', 'A4_Par4(100m)', 
-             'A5_Par5(130m)', 'A6_Par4(72m)', 'A7_Par4(81m)', 'A8_Par3(50m)', 'A9_Par3(50m)', 
-             'B1_Par3(69m)', 'B2_Par4(100m)', 'B3_Par4(68m)', 'B4_Par3(50m)', 
+    holes = ['A1_Par4(60m)', 'A2_Par3(40m)', 'A3_Par3(65m)', 'A4_Par4(100m)',
+             'A5_Par5(130m)', 'A6_Par4(72m)', 'A7_Par4(81m)', 'A8_Par3(50m)', 'A9_Par3(50m)',
+             'B1_Par3(69m)', 'B2_Par4(100m)', 'B3_Par4(68m)', 'B4_Par3(50m)',
              'B5_Par4(58m)', 'B6_Par3(55m)', 'B7_Par4(71m)', 'B8_Par5(123m)', 'B9_Par3(66m)',
-             'C1_Par4(60m)', 'C2_Par3(40m)', 'C3_Par3(65m)', 'C4_Par4(100m)', 
+             'C1_Par4(60m)', 'C2_Par3(40m)', 'C3_Par3(65m)', 'C4_Par4(100m)',
              'C5_Par5(130m)', 'C6_Par4(72m)', 'C7_Par4(81m)', 'C8_Par3(50m)', 'C9_Par3(50m)',
-             'D1_Par3(69m)', 'D2_Par4(100m)', 'D3_Par4(68m)', 'D4_Par3(50m)', 
+             'D1_Par3(69m)', 'D2_Par4(100m)', 'D3_Par4(68m)', 'D4_Par3(50m)',
              'D5_Par4(58m)', 'D6_Par3(55m)', 'D7_Par4(71m)', 'D8_Par5(123m)', 'D9_Par3(66m)']
-
-    # 홀 선택
-    selected_hole = st.sidebar.radio("홀 선택", ['A홀', 'B홀', 'C홀', 'D홀', '전체'])
 
     # 스코어카드 생성
     if 'scorecard' not in st.session_state:
-        st.session_state['scorecard'] = pd.DataFrame(index=players, columns=holes)
+        st.session_state['scorecard'] = pd.DataFrame(index=players, columns=holes).astype(int)
 
     scorecard = st.session_state['scorecard']
 
-    if selected_hole == '전체':
-        selected_holes = holes
-    elif selected_hole == 'A홀':
+    # 홀 선택
+    hole_selection = st.sidebar.radio('홀 선택', ['A홀', 'B홀', 'C홀', 'D홀', '전체'])
+
+    if hole_selection == 'A홀':
         selected_holes = holes[:9]
-    elif selected_hole == 'B홀':
+    elif hole_selection == 'B홀':
         selected_holes = holes[9:18]
-    elif selected_hole == 'C홀':
+    elif hole_selection == 'C홀':
         selected_holes = holes[18:27]
-    elif selected_hole == 'D홀':
+    elif hole_selection == 'D홀':
         selected_holes = holes[27:]
+    else:
+        selected_holes = holes
 
     # 사용자에게 입력 받기
     for hole in selected_holes:
         st.subheader(hole)
         for player in players:
-            default_value = scorecard.loc[player, hole] if (player in scorecard.index) and (hole in scorecard.columns) and not pd.isna(scorecard.loc[player, hole]) else 0
+            default_value = scorecard.loc[player, hole] if not pd.isna(scorecard.loc[player, hole]) else 0
             score = st.number_input(f'{player} {hole} 점수', min_value=0, value=int(default_value), key=f'{player}_{hole}', format="%d")
             scorecard.loc[player, hole] = score
 
     # 입력 받은 정보를 표시하고 다운로드 받기
     if st.button('제출'):
-        summary = pd.DataFrame(index=players, columns=['TTL', 'A', 'B', 'C', 'D', 'A_Dif', 'B_Dif', 'C_Dif', 'D_Dif', 'TTL_Dif'])
+        summary = pd.DataFrame(index=players, columns=['TTL', 'A', 'B', 'C', 'D'])
         summary['A'] = scorecard.iloc[:, :9].sum(axis=1)
         summary['B'] = scorecard.iloc[:, 9:18].sum(axis=1)
         summary['C'] = scorecard.iloc[:, 18:27].sum(axis=1)
         summary['D'] = scorecard.iloc[:, 27:].sum(axis=1)
-        summary['TTL'] = summary['A'] + summary['B'] + summary['C'] + summary['D']
+        summary['TTL'] = summary.sum(axis=1)
 
-        # 차이를 계산
-        summary['A_Dif'] = summary['A'] - 33
-        summary['B_Dif'] = summary['B'] - 33
-        summary['C_Dif'] = summary['C'] - 33
-        summary['D_Dif'] = summary['D'] - 33
-        summary['TTL_Dif'] = summary['TTL'] - 132
-
-        st.write(summary.fillna(0).astype(int))
-        st.write(scorecard.fillna(0).astype(int))
+        st.write(summary)
+        st.write(scorecard)
 
         full_scorecard = pd.concat([summary, scorecard], axis=1)
 
