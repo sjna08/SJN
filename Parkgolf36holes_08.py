5910347c5cd004369550d5d435e2b4c1c9e782b7
sjna08 <132455968+sjna08@users.noreply.github.com>
2023-06-10 09:54:27 +0900

    Update Parkgolf36holes_08.py

--- Parkgolf36holes_08.py
+++ Parkgolf36holes_08.py
@@ -72,7 +72,7 @@ def app():
         full_scorecard = pd.concat([summary, scorecard], axis=1)
 
         csv_buffer = io.StringIO()
-        full_scorecard.to_csv(csv_buffer, index=True, encoding= 'utf-8(BOM)')
+        full_scorecard.to_csv(csv_buffer, index=True, encoding= 'utf-8-sig')
 
         csv_string = csv_buffer.getvalue()
         b64 = base64.b64encode(csv_string.encode()).decode()
