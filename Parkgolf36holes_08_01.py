3c93da299f4cfe8e245e7f88b50193347b57f10f
sjna08 <132455968+sjna08@users.noreply.github.com>
2023-06-10 18:59:30 +0900

    Update Parkgolf36holes_08_01.py

--- Parkgolf36holes_08_01.py
+++ Parkgolf36holes_08_01.py
@@ -49,7 +49,7 @@ def app():
        st.markdown(hole_name, unsafe_allow_html=True)
        # st.subheader(hole)
         
-        for player in players:
+       for player in players:
             default_value = scorecard.loc[player, hole] if (player in scorecard.index) and (hole in scorecard.columns) and not pd.isna(scorecard.loc[player, hole]) else 0
             score = st.number_input(f'{player} {hole} 점수', min_value=0, value=int(default_value), key=f'{player}_{hole}', format="%d")
             scorecard.loc[player, hole] = score
