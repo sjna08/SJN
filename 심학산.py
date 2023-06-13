77d084551695562a3a00d1b274e5c1f884d59abc
sjna08 <132455968+sjna08@users.noreply.github.com>
2023-06-13 18:36:08 +0900

    Update 심학산.py

--- 심학산.py
+++ 심학산.py
@@ -8,14 +8,14 @@ def app():
     st.title("ParkGolf ScoreCard")
 
     # 홀 이름과 거리 설정
-    holes = ['A1_Par4(60m)', 'A2_Par3(40m)', 'A3_Par3(65m)', 'A4_Par4(100m)', 
-            'A5_Par5(130m)', 'A6_Par4(72m)', 'A7_Par4(81m)', 'A8_Par3(50m)', 'A9_Par3(50m)', 
-            'B1_Par3(69m)', 'B2_Par4(100m)', 'B3_Par4(68m)', 'B4_Par3(50m)', 
-            'B5_Par4(58m)', 'B6_Par3(55m)', 'B7_Par4(71m)', 'B8_Par5(123m)', 'B9_Par3(66m)',
-            'C1_Par4(60m)', 'C2_Par3(40m)', 'C3_Par3(65m)', 'C4_Par4(100m)', 
-            'C5_Par5(130m)', 'C6_Par4(72m)', 'C7_Par4(81m)', 'C8_Par3(50m)', 'C9_Par3(50m)',
-            'D1_Par3(69m)', 'D2_Par4(100m)', 'D3_Par4(68m)', 'D4_Par3(50m)', 
-            'D5_Par4(58m)', 'D6_Par3(55m)', 'D7_Par4(71m)', 'D8_Par5(123m)', 'D9_Par3(66m)']
+    holes = ['A1_Par4(62m)', 'A2_Par3(41m)', 'A3_Par4(67m)', 'A4_Par3(40m)', 
+            'A5_Par5(100m)', 'A6_Par3(52m)', 'A7_Par4(60m)', 'A8_Par3(40m)', 'A9_Par4(62m)', 
+            'B1_Par4(68m)', 'B2_Par4(62m)', 'B3_Par3(41m)', 'B4_Par5(100m)', 
+            'B5_Par3(50m)', 'B6_Par3(52m)', 'B7_Par4(72m)', 'B8_Par3(40m)', 'B9_Par4(67m)',
+            'C1_Par4(62m)', 'C2_Par3(41m)', 'C3_Par4(67m)', 'C4_Par3(40m)', 
+            'C5_Par5(100m)', 'C6_Par3(52m)', 'C7_Par4(60m)', 'C8_Par3(40m)', 'C9_Par4(62m)',
+            'D1_Pa4(68m)', 'D2_Par4(62m)', 'D3_Par3(41m)', 'D4_Par5(100m)', 
+            'D5_Par3(50m)', 'D6_Par3(52m)', 'D7_Par4(72m)', 'D8_Par3(40m)', 'D9_Par4(67m)']
 
     num_players = 4
     players = [st.sidebar.text_input(f'Player {i+1} 이름', value=f'Player{i+1}') for i in range(num_players)]
