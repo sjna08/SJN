86ab8f421834aa00e56f81374e48733b03370b41
sjna08 <132455968+sjna08@users.noreply.github.com>
2023-06-14 22:42:26 +0900

    Update YDP36_download(01).py

--- YDP36_download(01).py
+++ YDP36_download(01).py
@@ -49,18 +49,18 @@ def app():
 
     # Keep a reference to scorecard (do not get from session state again in this function)
     scorecard = st.session_state['scorecard']
+
     # Display scorecard for input
-    scorecard = st.session_state['scorecard']
     for hole in selected_holes:
         hole_name = f'<h3><strong>{hole}</strong></h3>'
         st.markdown(hole_name, unsafe_allow_html=True)
 
         for player in players:
-            score_key = f'{player}_{hole}'
-            default_value = st.session_state.get(score_key, 0)  # Use get method to avoid key error
-            score = st.number_input(f'{player} {hole} 점수', min_value=0, value=int(default_value), key=score_key, format="%d")
-            st.session_state[score_key] = score  # Update score in session state
-            
+            default_value = scorecard.loc[player, hole] if not pd.isna(scorecard.loc[player, hole]) else 0
+            score = st.number_input(f'{player} {hole} 점수', min_value=0, value=int(default_value), key=f'{player}_{hole}', format="%d")
+            scorecard.loc[player, hole] = score  # Update score in the scorecard DataFrame directly
+
+           
     # Store the whole scorecard in session state
     st.session_state['scorecard'] = scorecard
 
