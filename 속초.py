22a4894a761e5d7b22f410d41b757709898290cd
sjna08 <132455968+sjna08@users.noreply.github.com>
2023-06-22 01:30:58 +0900

    Update 속초.py

--- 속초.py
+++ 속초.py
@@ -1,20 +1,39 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+import sqlite3
 import base64
 import io
 import datetime
 
 def app():
+    conn = sqlite3.connect(':memory:', check_same_thread=False)
+
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS scorecard (
+            player TEXT,
+            hole TEXT,
+            score INTEGER,
+            PRIMARY KEY (player, hole)
+        )
+    """)
+
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS players (
+            name TEXT
+        )
+    """)
+
+    conn.execute("DELETE FROM players")
+    conn.commit()
+
     st.title("속초 ScoreCard")
 
-    # 현재 날짜 가져오기
     current_date = datetime.date.today()
 
-    # Streamlit 앱에 현재 날짜 출력, 년-월-일 형식으로
     st.subheader(current_date.strftime('%Y년 %m월 %d일'))
 
-    # 홀 이름과 거리 설정
+ 
+      # 홀 이름과 거리 설정
     holes = ['A1_Par3(58m)', 'A2_Par4(65m)', 'A3_Par3(53m)', 'A4_Par4(72m)', 
             'A5_Par4(90m)', 'A6_Par5(120m)', 'A7_Par3(45m)', 'A8_Par3(50m)', 'A9_Par4(96m)', 
             'B1_Par4(72m)', 'B2_Par4(68m)', 'B3_Par5(120m)', 'B4_Par3(60m)', 
@@ -24,8 +43,12 @@ def app():
             'D1_Par4(72m)', 'D2_Par4(68m)', 'D3_Par5(120m)', 'D4_Par3(60m)', 
             'D5_Par3(58m)', 'D6_Par3(58m)', 'D7_Par4(70m)', 'D8_Par4(70m)', 'D9_Par3(50m)']
 
+    
     num_players = 4
     players = [st.sidebar.text_input(f'Player {i+1} 이름', value=f'Player{i+1}') for i in range(num_players)]
+    for player in players:
+        conn.execute("INSERT INTO players (name) VALUES (?)", (player,))
+        conn.commit()
 
     page = st.sidebar.radio('페이지 선택', ['A&B 홀', 'C&D 홀', '전체'])
 
@@ -36,42 +59,30 @@ def app():
     else:
         selected_holes = holes
 
-   # 스코어카드 생성
-    scorecard = pd.DataFrame(index=players, columns=holes)
-
-    # Create or load scorecard
-    if 'scorecard' not in st.session_state:
-        st.session_state['scorecard'] = pd.DataFrame(index=players, columns=holes)
-    elif st.button("저장된 점수카드 불러오기"):
-        try:
-            st.session_state['scorecard'] = pd.read_excel('scorecard.xlsx', index_col=0)
-        except FileNotFoundError:
-            st.warning("저장된 점수카드를 찾을 수 없습니다.")
-
-    # Update player names
-    st.session_state['scorecard'].index = players
-
-    # Display scorecard for input
-    scorecard = st.session_state['scorecard']
     for hole in selected_holes:
-       hole_name = f'<h3><strong>{hole}</strong></h3>'
-       st.markdown(hole_name, unsafe_allow_html=True)
-        
-       for player in players:
-            default_value = scorecard.loc[player, hole] if not pd.isna(scorecard.loc[player, hole]) else 0
-            score = st.number_input(f'{player} {hole} 점수', min_value=0, value=int(default_value), key=f'{player}_{hole}', format="%d")
-            scorecard.loc[player, hole] = score
+        hole_name = f'<h3><strong>{hole}</strong></h3>'
+        st.markdown(hole_name, unsafe_allow_html=True)
 
-    # Update scorecard in session state
-    st.session_state['scorecard'] = scorecard
+        for player in players:
+            score = conn.execute("SELECT score FROM scorecard WHERE player=? AND hole=?", (player, hole)).fetchone()
+            score = score[0] if score else 0
+            score = st.number_input(f'{player} {hole} 점수', min_value=0, value=int(score), key=f'{player}_{hole}', format="%d")
+            
+            conn.execute("""
+                INSERT OR REPLACE INTO scorecard (player, hole, score)
+                VALUES (?, ?, ?)
+            """, (player, hole, score))
+            conn.commit()
 
-    # Submit scores and calculate summary
     if st.button('제출'):
-        summary = pd.DataFrame(index=players, columns=['TTL','A','B', 'A_Dif','B_Dif','C','D', 'C_Dif','D_Dif','TTL_Dif'])
-        summary['A'] = scorecard.iloc[:, :9].sum(axis=1)
-        summary['B'] = scorecard.iloc[:, 9:18].sum(axis=1)
-        summary['C'] = scorecard.iloc[:, 18:27].sum(axis=1)
-        summary['D'] = scorecard.iloc[:, 27:].sum(axis=1)
+        df = pd.read_sql_query("SELECT * from scorecard", conn)
+        df = df.pivot(index='player', columns='hole', values='score')
+
+        summary = pd.DataFrame(index=players, columns=['TTL','A','B','C','D', 'A_Dif','B_Dif','C_Dif','D_Dif','TTL_Dif'])
+        summary['A'] = df.iloc[:, :9].sum(axis=1)
+        summary['B'] = df.iloc[:, 9:18].sum(axis=1)
+        summary['C'] = df.iloc[:, 18:27].sum(axis=1)
+        summary['D'] = df.iloc[:, 27:].sum(axis=1)
         summary['TTL'] = summary['A'] + summary['B'] + summary['C'] + summary['D']
 
         summary['A_Dif'] = summary['A'] - 33
@@ -80,31 +91,26 @@ def app():
         summary['D_Dif'] = summary['D'] - 33
         summary['TTL_Dif'] = summary['TTL'] - 132
 
-        st.write(summary.fillna(0).astype(int))
-        st.write(scorecard.fillna(0).astype(int))
+        full_df = pd.concat([summary, df], axis=1).fillna(0).astype(int)
 
-        full_scorecard = pd.concat([summary, scorecard], axis=1)
+        st.write(full_df)
 
-         # Excel 파일을 메모리에 저장하기 위한 버퍼를 생성합니다.
         excel_buffer = io.BytesIO()
         
-        # DataFrame을 Excel 파일로 저장합니다.
         with pd.ExcelWriter(excel_buffer, engine='openpyxl') as writer:
-            full_scorecard.to_excel(writer, index=True)
+            full_df.to_excel(writer, index=True)
 
-        # 버퍼에 저장된 데이터를 가져옵니다.
         excel_data = excel_buffer.getvalue()
 
-     # st.download_button을 이용해 excel 파일을 다운로드 링크로 제공합니다.
         st.download_button(label='점수카드 다운로드',
-                            data=excel_data,
-                            file_name='scorecard.xlsx',
-                            mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+                        data=excel_data,
+                        file_name=f'점수카드_{current_date.strftime("%Y%m%d")}.xlsx',
+                        mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    
+        conn.execute("DELETE FROM scorecard")
+        conn.commit()
 
-        if st.button("점수카드 저장"):
-            with open('scorecard.xlsx', 'wb') as f:
-                f.write(excel_data)
-
+    conn.close()
 
 if __name__ == "__main__":
     app()
